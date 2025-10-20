2a2d78ffa7baac98be6c7841431daecae1931a83
mertcanisik123-a11y <mertcanisik.123@gmail.com>
2025-10-20 15:16:38 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  })
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // kullanıyorsan şunları da aç:
+      // '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      // '@lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+    },
+  },
+})
